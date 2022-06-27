07d7a6474261eb1f922522fd861d61e53e819124
rafaelwake <rafaelwake07@gmail.com>
2022-06-27 11:30:30 -0300

    aula 5 - concluido

---
+++ main.js
@@ -0,0 +1,32 @@
+function tocaSom(seletorAudio) {
+  const elemento = document.querySelector(seletorAudio);
+
+  if (elemento && elemento.localName === "audio") {
+    elemento.play();
+  } else {
+    console.log("não encontrado");
+  }
+}
+
+const listaDeTeclas = document.querySelectorAll(".tecla");
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+  const tecla = listaDeTeclas[i];
+  const instrument = tecla.classList[1];
+
+  //template string
+  const idAudio = `#som_${instrument}`;
+
+  listaDeTeclas[i].onclick = function () {
+    tocaSom(idAudio);
+    console.log(instrument);
+  };
+  tecla.onkeydown = function (evento) {
+    if (evento.code === "Space" || evento.code === "Enter") {
+      tecla.classList.add("ativa");
+    }
+  };
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa");
+  };
+}
